b5587402ae91064ebc52be2f1fa2177baea4bfc3
davaa <davaalikaramadhani@gmail.com>
2026-03-05 16:34:36 +0700

    commit

--- resources/views/components/navigation/navbar.blade.php
+++ resources/views/components/navigation/navbar.blade.php
@@ -6,10 +6,19 @@
     <nav>
         <ul class="flex justify-between space-x-15 items-center">
             <li>
-                <div>
-                    <a href="{{ route('index') }}">
-                        <h1 class="text-xl font-medium">Perpustakaan Saya</h1>
-                    </a>
+                <div class="flex space-x-10 text-lg w-6xl">
+                    <div class="w-70">
+                        <a href="{{ route('index') }}">
+                            <h1 class="mt-1">Perpustakaan Saya</h1>
+                        </a>
+                    </div>
+                    <form action="" class="relative w-full">
+                        <x-search-input></x-search-input>
+                    </form>
+                    <a class="mt-1" href="">Kategori</a>
+                    @if (Auth::check())
+                        <a class="mt-1" href="{{ route('peminjaman.index') }}">Peminjaman</a>
+                    @endif
                 </div>
             </li>
             <li>
@@ -19,8 +28,9 @@
                             @csrf
                             <button type="submit">Logout</button>
                         </form>
-                        <a class="flex gap-x-1" href="">
-                            <i class="size-6" data-lucide="user"></i>
+                        <a href="">
+                            <p class="text-lg/tight">{{ Auth::user()->username ?? 'User' }}</p>
+                            <p class="text-sm/tight hover:underline">{{ Auth::user()->email ?? 'user@example.com' }}</p>
                         </a>
                         <a hidden class="py-1 px-4 text-indigo-500 outline-2 outline-indigo-500 rounded-lg text-center"
                             href="{{ route('auth.register') }}">Register</a>
@@ -28,9 +38,10 @@
                             class="py-1 px-6 text-white bg-indigo-500 outline-2 outline-indigo-500 rounded-lg text-center"
                             href="{{ route('login') }}">Login</a>
                     @else
-                        <a class="text-black font-medium text-sm" href="{{ route('login') }}">Login</a>
-                        <a class="py-2 px-4 bg-(--third-color) text-sm font-medium text-(--primary-color) rounded-md"
+                        <a class="py-1 px-4 text-indigo-500 outline-2 outline-indigo-500 rounded-lg text-center"
                             href="{{ route('auth.register') }}">Register</a>
+                        <a class="py-1 px-6 text-white bg-indigo-500 outline-2 outline-indigo-500 rounded-lg text-center"
+                            href="{{ route('login') }}">Login</a>
                     @endif
                 </div>
             </li>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,75 +1,32 @@
 <x-layouts.user-dashboard>
-    <section class="py-20 px-4">
-        <div class="container mx-auto text-center max-w-4xl">
-            <h1 class="text-black text-6xl font-medium mb-6">Lorem ipsum dolor adipisicing.</h1>
-            <p class="mb-8 max-w-2xl mx-auto text-xl text-black/80">Lorem ipsum dolor, sit amet
-                consectetur adipisicing
-                elit. Hic,
-                dolorum
-                omnis. Enim
-                et obcaecati eligendi
-                error fuga nostrum ducimus. Fugit neque consectetur laudantium sequi in voluptatibus, repudiandae
-                explicabo eveniet accusantium!</p>
-            <div class="mt-10 grid grid-cols-3 gap-8 max-w-2xl mx-auto">
-                <div>
-                    <h2 class="text-(--third-color) font-medium" style="font-size: 2.5rem">10K</h2>
-                    <h3 class="capitalize text-base text-black/70">new this month</h3>
-                </div>
-                <div>
-                    <h2 class="text-(--third-color) font-medium" style="font-size: 2.5rem">10K</h2>
-                    <h3 class="capitalize text-base text-black/70">new this month</h3>
-                </div>
-                <div>
-                    <h2 class="text-(--third-color) font-medium" style="font-size: 2.5rem">10K</h2>
-                    <h3 class="capitalize text-base text-black/70">new this month</h3>
-                </div>
-            </div>
-        </div>
-    </section>
-    <section class="py-16 px-4 bg-white">
-        <div class="container mx-auto">
-            <div class="mb-8">
-                <h2 class="text-xl font-medium capitalize">Eksplor Buku</h2>
-                <h2 class="text-md text-black/60 mt-2">Eksplor buku koleksi kami</h2>
-            </div>
-            <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-                @forelse ($books as $book)
-                    <a href="{{ route('book.show', $book->id) }}">
-                        <div
-                            class="group cursor-pointer rounded-xl overflow-hidden shadow-sm hover:shadow-xl transition-all duration-300">
-                            <div class="relative aspect-3/4 overflow-hidden">
-                                <img class="w-full h-full object-cover group-hover:scale-105 transition-transform duration-300"
-                                    src="{{ asset('storage/' . $book->cover_buku) }}" alt="cover">
-                            </div>
-                            <div class="p-5">
-                                @php
-                                    $category = $book->kategoriBukuRelasi->pluck('kategori.nama_kategori')->toArray();
-                                @endphp
-                                <p class="text-sm text-black/60">
-                                    {{ implode(', ', $category) }}
-                                </p>
-                                <h3 class="mb-1 line-clamp-2 text-lg font-medium">
-                                    {{ $book->judul }}</h3>
-                                <p class="mb-3 text-base text-black/80">{{ $book->penulis }}</p>
-                                <div class="flex items-center gap-2 mb-4">
-                                    <div class="flex items-center gap-1">
-                                        <i data-lucide="star"
-                                            class="size-5 fill-(--third-color) stroke-1 stroke-(--third-color)"></i>
-                                        @php
-                                            $avg = $book->averageRating();
-                                        @endphp
+    <div class="flex gap-5 flex-wrap w-full">
+        @forelse ($books as $book)
+            <a href="{{ route('book.show', $book->id) }}">
+                <div class="w-70 sm:w-45 rounded-lg px-2 py-4 shadow-md/30 bg-white">
+                    <img class="border h-40 object-cover rounded-lg mx-auto border-none"
+                        src="{{ asset('storage/' . $book->cover_buku) }}" alt="cover">
+                    <div>
+                        <h1 class="font-medium text-lg mt-2 whitespace-nowrap overflow-hidden text-ellipsis">
+                            {{ $book->judul }}</h1>
+                        <h2 class="text-base">{{ $book->penulis }}</h2>
+                        <div class="flex space-x-1.5 items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="yellow" viewBox="0 0 24 24" stroke-width="1.5"
+                                stroke="currentColor" class="size-5 stroke-none">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
+                            </svg>
+                            @php
+                                $avg = $book->averageRating();
+                            @endphp
 
-                                        <p class="text-sm">{{ $avg }}</p>
-                                    </div>
-                                    <p class="text-sm text-black/60">({{ $book->ulasan->count() }})</p>
-                                </div>
-                            </div>
+                            <p class="text-sm">{{ $avg }}</p>
+
                         </div>
-                    </a>
-                @empty
-                    <h1>Tidak Ada Data</h1>
-                @endforelse
-            </div>
-        </div>
-    </section>
+                    </div>
+                </div>
+            </a>
+        @empty
+            <h1>Tidak Ada Data</h1>
+        @endforelse
+    </div>
 </x-layouts.user-dashboard>
