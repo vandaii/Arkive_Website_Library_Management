8aa19cf885b6e13584c4ee2ae28a102218739464
davaa <davaalikaramadhani@gmail.com>
2026-04-07 07:10:36 +0700

    apply middleware multiple role

--- app/Http/Middleware/CheckRole.php
+++ app/Http/Middleware/CheckRole.php
@@ -16,7 +16,7 @@ class CheckRole
      */
     public function handle(Request $request, Closure $next, ...$roles): Response
     {
-        if (Auth::check()) {
+        if (!Auth::check()) {
             return redirect('/login');
         }
 
